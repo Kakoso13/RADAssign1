e275bc63fa8bff1de97cb98cbc22887a46110ee2
Kakoso13 <cassio.torquetti@gmail.com>
2014-10-09 23:48:54 -0400

    created vb auto center form

---
+++ VBAutoCenter/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VBAutoCenter
+{
+    public partial class VBAutoCenterForm : Form
+    {
+        public VBAutoCenterForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
